209349c494b530715291306f763ad4207ba3fff5
Tamás Tardos <tatardos@gmail.com>
2018-01-20 20:37:34 +0000

    Spawning (and respawning added)

--- Assets/Scripts/SimParameters.cs
+++ Assets/Scripts/SimParameters.cs
@@ -3,5 +3,11 @@
 public class SimParameters
 {
 	public static readonly float CONTROLLER_UPDATE_RATE = 0.1f;
+	public static readonly float MAX_TOTAL_RADIUS_SQUARED = 10f;
+	public static readonly float MAP_WIDTH = 10f;
+	public static readonly float MAP_HEIGHT = 10f;
+	public static readonly float MIN_SPAWN_RADIUS = 0.5f;
+	public static readonly float MAX_SPAWN_RADIUS = 1f;
+	public static readonly float SPRITE_SCALAR = 5f;
 }
 
--- Assets/Scripts/SpawnHandler.cs
+++ Assets/Scripts/SpawnHandler.cs
@@ -6,17 +6,44 @@ public class SpawnHandler : MonoBehaviour {
 
 	public static string[] AIScripts;
 
+	[SerializeField]
+	public GameObject prefab;
+
+	private int myNumAITypes;
+	private int currentAIType = 0;
+
+	private float myTotalRadiusSquared = 0;
+
 	// Use this for initialization
 	void Start () {
 		Object[] texts = Resources.LoadAll("AIs/", typeof(TextAsset));
-		AIScripts = new string[texts.Length];
-		for (int i = 0; i < texts.Length; i++) {
+		myNumAITypes = texts.Length;
+		AIScripts = new string[myNumAITypes];
+		for (int i = 0; i < myNumAITypes; i++) {
 			AIScripts [i] = ((TextAsset)(texts [i])).text;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		while (myTotalRadiusSquared < SimParameters.MAX_TOTAL_RADIUS_SQUARED) {
+			float newRadius = SimParameters.MIN_SPAWN_RADIUS + Random.value * (SimParameters.MAX_SPAWN_RADIUS - SimParameters.MIN_SPAWN_RADIUS);
+			float newX = Random.value * (SimParameters.MAP_WIDTH) - SimParameters.MAP_WIDTH;
+			float newY = Random.value * (SimParameters.MAP_HEIGHT) - SimParameters.MAP_HEIGHT;
+			GameObject cell = Instantiate (prefab, new Vector3 (newX, newY, 0), Quaternion.identity);
+			cell.transform.localScale = (new Vector3(newRadius, newRadius, newRadius)) * SimParameters.SPRITE_SCALAR;
+			cell.AddComponent(typeof(Movement));
+			addRadius(newRadius);
+			currentAIType = (currentAIType + 1) % myNumAITypes; // cycling through AI types
+		}
 	}
+
+	public void addRadius(float r) {
+		myTotalRadiusSquared += r * r;
+	}
+	public void removeRadius(float r) {
+		myTotalRadiusSquared -= r * r;
+	}
+
 }
+
